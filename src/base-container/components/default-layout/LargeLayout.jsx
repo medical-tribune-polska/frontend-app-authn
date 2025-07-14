457da818a811a56ae3a2adc680eef970b14d2b4b
Rafał Tulwin <rtulwin@gmail.com>
2025-07-14 21:16:56 +0200

    Changing style

--- src/base-container/components/default-layout/LargeLayout.jsx
+++ src/base-container/components/default-layout/LargeLayout.jsx
@@ -17,7 +17,7 @@ const LargeLayout = () => {
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
           <Image className="logo position-absolute" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
         </Hyperlink>
-        <div className="min-vh-100 d-flex align-items-center">
+        <div className="min-vh-100 d-flex align-items-center justify-content-sm-center">
           <div className={classNames({ 'large-yellow-line mr-n4.5': getConfig().SITE_NAME === 'edX' })} />
           <h1
 //             className={classNames(
