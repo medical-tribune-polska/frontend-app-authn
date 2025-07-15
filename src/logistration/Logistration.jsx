aa76105dd5ff46bcd77934e387099a9af2d86d78
Rafał Tulwin <rtulwin@gmail.com>
2025-07-15 15:31:02 +0200

    Changing style

--- src/logistration/Logistration.jsx
+++ src/logistration/Logistration.jsx
@@ -107,7 +107,7 @@ const Logistration = (props) => {
                   <Tab title={tabTitle} eventKey={LOGIN_PAGE} />
                 </Tabs>
               )}
-              <div id="main-content" className="main-content test-background">
+              <div id="main-content" className="main-content">
                 {!institutionLogin && (
                   <h3 className="mb-4.5 text-medium-black">{formatMessage(messages['logistration.sign.in'])}</h3>
                 )}
