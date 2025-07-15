19d769d714a0c10aa52ea91fbeff7c9fbe3f7b6e
Rafał Tulwin <rtulwin@gmail.com>
2025-07-15 21:21:29 +0200

    Changing style

--- src/base-container/index.jsx
+++ src/base-container/index.jsx
@@ -17,7 +17,7 @@ const BaseContainer = ({ children, showWelcomeBanner, fullName }) => {
 
   if (enableImageLayout) {
     return (
-      <div className="layout">
+      <div className="layout right-to-left-gradient">
         <MediaQuery maxWidth={breakpoints.extraSmall.maxWidth - 1}>
           {showWelcomeBanner ? <AuthSmallLayout fullName={fullName} /> : <ImageExtraSmallLayout />}
         </MediaQuery>
@@ -38,7 +38,7 @@ const BaseContainer = ({ children, showWelcomeBanner, fullName }) => {
   }
 
   return (
-    <>
+{/*     <> */}
 {/*       <div className="col-md-12 extra-large-screen-top-stripe" /> */}
       <div className="layout right-to-left-gradient">
         <MediaQuery maxWidth={breakpoints.small.maxWidth - 1}>
@@ -54,7 +54,7 @@ const BaseContainer = ({ children, showWelcomeBanner, fullName }) => {
           {children}
         </div>
       </div>
-    </>
+//     </>
   );
 };
 
