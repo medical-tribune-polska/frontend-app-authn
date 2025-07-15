5d39c32270ca4d3f8370d35ae9b2dbe816181954
Rafał Tulwin <rtulwin@gmail.com>
2025-07-15 10:37:19 +0200

    Changing style

--- src/base-container/components/default-layout/LargeLayout.jsx
+++ src/base-container/components/default-layout/LargeLayout.jsx
@@ -11,36 +11,22 @@ const LargeLayout = () => {
   const { formatMessage } = useIntl();
 
   return (
-//     <div className="w-50 d-flex">
-    <div className="justify-center">
-{/*       <div className="col-md-9 bg-primary-400"> */}
+    <div>
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
           <Image className="position-absolute logo-margin" alt={getConfig().SITE_NAME} src="../../../style/assets/logo.png" />
         </Hyperlink>
-        <div className="min-vh-100 d-flex align-items-center large-text-container">
-          <div className={classNames({ 'large-yellow-line mr-n4.5': getConfig().SITE_NAME === 'edX' })} />
-          <h1
-//             className={classNames(
-//               'display-2 mw-xs',
-//               { 'ml-6': getConfig().SITE_NAME !== 'edX' },
-//             )}
-             className="text-large-gray"
-          >
-            {formatMessage(messages['start.learning'])}
-            <div className="text-large-black">
-              {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
+        <div className="justify-center">
+            <div className="min-vh-100 d-flex align-items-center large-text-container">
+{/*               <div className={classNames({ 'large-yellow-line mr-n4.5': getConfig().SITE_NAME === 'edX' })} /> */}
+              <h1 className="text-large-gray" >
+                {formatMessage(messages['start.learning'])}
+                <div className="text-large-black">
+                  {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
+                </div>
+              </h1>
             </div>
-          </h1>
         </div>
-{/*       </div> */}
-{/*       <div className="col-md-3 bg-white p-0"> */}
-{/*         <svg className="ml-n1 w-100 h-100 large-screen-svg-primary" preserveAspectRatio="xMaxYMin meet"> */}
-{/*           <g transform="skewX(171.6)"> */}
-{/*             <rect x="0" y="0" height="100%" width="100%" /> */}
-{/*           </g> */}
-{/*         </svg> */}
-{/*       </div> */}
-    </div>
+   </div>
   );
 };
 
