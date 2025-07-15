8da1bfa4ab01513289a91266d84017981ff8e38b
Rafał Tulwin <rtulwin@gmail.com>
2025-07-15 16:11:13 +0200

    Changing style

--- src/logistration/Logistration.jsx
+++ src/logistration/Logistration.jsx
@@ -98,7 +98,7 @@ const Logistration = (props) => {
 
   return (
     <BaseContainer>
-      <div className="right-main-container justify-center">
+      <div className="right-main-container justify-center right-to-left-gradient">
 {/*         {disablePublicAccountCreation */}
 {/*           ? ( */}
 {/*             <> */}
