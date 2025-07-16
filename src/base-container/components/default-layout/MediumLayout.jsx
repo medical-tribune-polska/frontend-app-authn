b5a378a3b6cc245e000e5fe42da169536f79ebdd
Rafał Tulwin <rtulwin@gmail.com>
2025-07-16 08:53:10 +0200

    Changing style

--- src/base-container/components/default-layout/MediumLayout.jsx
+++ src/base-container/components/default-layout/MediumLayout.jsx
@@ -11,36 +11,24 @@ const MediumLayout = () => {
   const { formatMessage } = useIntl();
 
   return (
-//     <>
-{/*       <div className="w-100 medium-screen-top-stripe" /> */}
-        <div className="w-100 p-0 mb-3 d-flex background-gradient">
-{/*         <div className="col-md-10 bg-primary-400"> */}
-          <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-            <Image alt={getConfig().SITE_NAME} className="logo" src={require("../../../style/assets/logo.png")} />
-          </Hyperlink>
-          <div className="d-flex align-items-center justify-content-center mb-4 ">
-            <div className={classNames({ 'mt-1 medium-yellow-line': getConfig().SITE_NAME === 'edX' })} />
-            <div>
-              <h1 className="text-large-gray" >
-                <span>
-                  {formatMessage(messages['start.learning'])}{' '}
-                  <span className="text-large-black">
-                    {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
-                  </span>
-                </span>
-              </h1>
-            </div>
-          </div>
+    <div className="w-100 p-0 mb-3 d-flex background-gradient">
+      <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
+        <Image alt={getConfig().SITE_NAME} className="logo" src={require("../../../style/assets/logo.png")} />
+      </Hyperlink>
+      <div className="d-flex align-items-center justify-content-center mb-4 ">
+        <div className={classNames({ 'mt-1 medium-yellow-line': getConfig().SITE_NAME === 'edX' })} />
+        <div>
+          <h1 className="text-large-gray" >
+            <span>
+              {formatMessage(messages['start.learning'])}{' '}
+              <span className="text-large-black">
+                {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
+              </span>
+            </span>
+          </h1>
         </div>
-{/*         <div className="col-md-2 bg-white p-0"> */}
-{/*           <svg className="w-100 h-100 medium-screen-svg-primary" preserveAspectRatio="xMaxYMin meet"> */}
-{/*             <g transform="skewX(168)"> */}
-{/*               <rect x="0" y="0" height="100%" width="100%" /> */}
-{/*             </g> */}
-{/*           </svg> */}
-{/*         </div> */}
-{/*       </div> */}
-{/*     </> */}
+      </div>
+    </div>
   );
 };
 
