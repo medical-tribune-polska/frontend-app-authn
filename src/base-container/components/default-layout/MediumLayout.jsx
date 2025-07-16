59cf4e0010df9ba7f4a08dd0bd62ce4feaae85f7
Rafał Tulwin <rtulwin@gmail.com>
2025-07-16 10:30:14 +0200

    Changing style

--- src/base-container/components/default-layout/MediumLayout.jsx
+++ src/base-container/components/default-layout/MediumLayout.jsx
@@ -15,23 +15,14 @@ const MediumLayout = () => {
       <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
         <Image alt={getConfig().SITE_NAME} className="position-absolute logo-margin" src={require("../../../style/assets/logo.png")} />
       </Hyperlink>
-      <div className="d-flex align-items-center justify-center mb-4">
+      <div className="d-flex align-items-center justify-center mb-4 text-container-top-margin">
         <div className="d-flex align-items-center large-text-container min-top-height">
-{/*           <h1 className="text-large-gray" > */}
-{/*             <span> */}
-{/*               {formatMessage(messages['start.learning'])}{' '} */}
-{/*               <span className="text-large-black"> */}
-{/*                 {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })} */}
-{/*               </span> */}
-{/*             </span> */}
-
-              <h1 className="text-large-gray" >
-                {formatMessage(messages['start.learning'])}
-                <div className="text-large-black">
-                  {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
-                </div>
-              </h1>
-{/*           </h1> */}
+          <h1 className="text-large-gray" >
+            {formatMessage(messages['start.learning'])}
+            <div className="text-large-black">
+              {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
+            </div>
+          </h1>
         </div>
       </div>
     </div>
