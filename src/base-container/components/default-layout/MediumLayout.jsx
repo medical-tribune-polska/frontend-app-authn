7e09ff748ee789a182bb3687e37baab8cc49a8bb
Rafał Tulwin <rtulwin@gmail.com>
2025-07-16 08:45:03 +0200

    Changing style

--- src/base-container/components/default-layout/MediumLayout.jsx
+++ src/base-container/components/default-layout/MediumLayout.jsx
@@ -11,25 +11,20 @@ const MediumLayout = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <>
-      <div className="w-100 medium-screen-top-stripe" />
-      <div className="w-100 p-0 mb-3 d-flex">
-        <div className="col-md-10 bg-primary-400">
+//     <>
+{/*       <div className="w-100 medium-screen-top-stripe" /> */}
+        <div className="w-100 p-0 mb-3 d-flex background-gradient">
+{/*         <div className="col-md-10 bg-primary-400"> */}
           <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-            <Image alt={getConfig().SITE_NAME} className="logo" src={getConfig().LOGO_WHITE_URL} />
+            <Image alt={getConfig().SITE_NAME} className="logo" src={require("../../../style/assets/logo.png")} />
           </Hyperlink>
           <div className="d-flex align-items-center justify-content-center mb-4 ">
             <div className={classNames({ 'mt-1 medium-yellow-line': getConfig().SITE_NAME === 'edX' })} />
             <div>
-              <h1
-                className={classNames(
-                  'display-1 text-white mt-5 mb-5 mr-2 main-heading',
-                  { 'ml-4.5': getConfig().SITE_NAME !== 'edX' },
-                )}
-              >
+              <h1 className="text-large-gray" >
                 <span>
                   {formatMessage(messages['start.learning'])}{' '}
-                  <span className="text-accent-a d-inline-block">
+                  <span className="text-large-black">
                     {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
                   </span>
                 </span>
@@ -37,15 +32,15 @@ const MediumLayout = () => {
             </div>
           </div>
         </div>
-        <div className="col-md-2 bg-white p-0">
-          <svg className="w-100 h-100 medium-screen-svg-primary" preserveAspectRatio="xMaxYMin meet">
-            <g transform="skewX(168)">
-              <rect x="0" y="0" height="100%" width="100%" />
-            </g>
-          </svg>
-        </div>
-      </div>
-    </>
+{/*         <div className="col-md-2 bg-white p-0"> */}
+{/*           <svg className="w-100 h-100 medium-screen-svg-primary" preserveAspectRatio="xMaxYMin meet"> */}
+{/*             <g transform="skewX(168)"> */}
+{/*               <rect x="0" y="0" height="100%" width="100%" /> */}
+{/*             </g> */}
+{/*           </svg> */}
+{/*         </div> */}
+{/*       </div> */}
+{/*     </> */}
   );
 };
 
