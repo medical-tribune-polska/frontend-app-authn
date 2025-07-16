51fe391fc0fec50a5cba0fa1f46ff2883e12dc61
Rafał Tulwin <rtulwin@gmail.com>
2025-07-16 11:40:56 +0200

    Changing style

--- src/base-container/components/default-layout/LargeLayout.jsx
+++ src/base-container/components/default-layout/LargeLayout.jsx
@@ -17,7 +17,7 @@ const LargeLayout = () => {
         </Hyperlink>
         <div className="justify-center">
             <div className="min-vh-100 d-flex large-text-container">
-              <h1 className="text-large-gray" >
+              <h1 className="text-large-gray text-container-big-top-margin" >
                 {formatMessage(messages['start.learning'])}
                 <div className="text-large-black">
                   {formatMessage(messages['with.polish.site.name'], { siteName: getConfig().SITE_NAME })}
